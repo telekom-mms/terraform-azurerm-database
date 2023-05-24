f7012459f20f0465a3a6e01942b3bc6cf37fc113
Michaela Mattes <m.mattes@telekom.de>
2023-05-24 15:34:58 +0200

    first setup

--- versions.tf
+++ versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "registry.terraform.io/hashicorp/azurerm"
+      version = "3.57.0"
+    }
   }
   required_version = ">=1.3"
 }
